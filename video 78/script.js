5e290b32dcece68e7422e898cae8d396db9f572b
Mantu Kumar Tiwari <mantu99739@gmail.com>
2025-06-21 14:36:05 +0530

    Hacking Terminal

--- video 78/script.js
+++ video 78/script.js
@@ -2,7 +2,26 @@ console.log("Hacking Stimulator practice by myself");
 let time = Math.floor(Math.random() * 7)
 console.log(time);
 
+function printScreen (message){
+    document.getElementById("output").innerHTML += message + "<br>";
+}
+
 setTimeout(() => {
-    console.log("Initializing Hacking");
-    
-}, time * 1000);
+    printScreen("Initializing Hacking");
+}, 1000);
+
+setTimeout(() => {
+    printScreen("Reading your files");
+}, 200);
+
+setTimeout(() => {
+        printScreen("Password file detected");
+}, 3000);
+
+setTimeout(() => {
+    printScreen("all Password sended");
+}, 4000);
+
+setTimeout(() => {
+    printScreen("Hacked!");
+}, 5000);
